d6f32476887385755f072e4ed83b6cec9b9f2700
Andrew Leamy <drewn7512@gmail.com>
2019-11-18 20:48:36 -0800

    Integrated database cache and observer into LifeCounterFragment

--- WinRate/app/src/main/java/com/ucsc/winrate/ui/lifeCounter/LifeCounterViewModel.java
+++ WinRate/app/src/main/java/com/ucsc/winrate/ui/lifeCounter/LifeCounterViewModel.java
@@ -1,19 +1,42 @@
 package com.ucsc.winrate.ui.lifeCounter;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class LifeCounterViewModel extends ViewModel{
+import com.ucsc.winrate.WinRateRepository;
+import com.ucsc.winrate.table_entities.OpponentProfile;
+
+import java.util.List;
+
+public class LifeCounterViewModel extends AndroidViewModel {
 
     private MutableLiveData<String> mText;
+    private WinRateRepository repository;
+    private LiveData<List<OpponentProfile>> allOpponentProfiles; //cached copy of database
 
-    public LifeCounterViewModel() {
+    public LifeCounterViewModel(@NonNull Application application) {
+        super(application);
         mText = new MutableLiveData<>();
         mText.setValue("Life Counter");
+        this.repository = new WinRateRepository(application);
+        this.allOpponentProfiles = repository.getAllOpponentProfiles();
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<java.util.List<OpponentProfile>> getAllOpponentProfiles(){
+        return this.allOpponentProfiles;
+    }
+
+    //This method shouldn't be necessary:
+    public void insert(OpponentProfile newProfile){
+        repository.insert(newProfile);
     }
 }
