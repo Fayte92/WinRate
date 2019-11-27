6bb080e5e47d2830d0008a28ccec11b2195a7cca
Andrew Leamy <drewn7512@gmail.com>
2019-11-26 17:08:43 -0800

    merged working statistics page onto master

--- WinRate/app/src/main/java/com/ucsc/winrate/ui/stats/StatsFragment.java
+++ WinRate/app/src/main/java/com/ucsc/winrate/ui/stats/StatsFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -12,24 +11,96 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.Uri;
+
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+
+
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+
+
+import com.github.mikephil.charting.utils.ColorTemplate;
+import java.util.ArrayList;
+import java.util.List;
 import com.ucsc.winrate.R;
 
 public class StatsFragment extends Fragment{
 
     private StatsViewModel statsViewModel;
+    private PieChart mChart;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         statsViewModel =
                 ViewModelProviders.of(this).get(StatsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_stats, container, false);
-        //final TextView textView = root.findViewById(R.id.text_stats);
-        //statsViewModel.getText().observe(this, new Observer<String>() {
-        //    @Override
-        //    public void onChanged(@Nullable String s) {
-        //        textView.setText(s);
-        //    }
-        //});
-        return root;
+
+        View v = inflater.inflate(R.layout.fragment_stats, container, false);
+
+        mChart = (PieChart) v.findViewById(R.id.pieChart);
+        mChart.getDescription().setText("WinRate Data");
+
+        mChart.setCenterText(generateCenterText());
+        mChart.setCenterTextSize(10f);
+
+        // radius of the center hole in percent of maximum radius
+        mChart.setHoleRadius(45f);
+        mChart.setTransparentCircleRadius(50f);
+
+        Legend l = mChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+
+        mChart.setData(grabWinRateData());
+
+        return v;
+    }
+
+    private SpannableString generateCenterText() {
+        SpannableString s = new SpannableString("Win Rate");
+        s.setSpan(new RelativeSizeSpan(2f), 0, 8, 0);
+        s.setSpan(new ForegroundColorSpan(Color.GRAY), 8, s.length(), 0);
+        return s;
+    }
+
+    private PieData grabWinRateData() {
+        float rainfall[] = {10f, 20f };
+        String monthNames[] = {"Win", "Loss"};
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for (int i = 0; i < rainfall.length; i++) {
+            pieEntries.add(new PieEntry(rainfall[i], monthNames[i]));
+        }
+        PieDataSet dataSet = new PieDataSet(pieEntries, "Overall Winrate");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataSet);
+        return data;
     }
 }
